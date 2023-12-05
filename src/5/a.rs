2485a55f75c40aa33bdfd8eae81a34504f6eb592
ricardo <rmohamedhoesein3@gmail.com>
2023-12-05 08:46:53 +0100

    Add day 5.

---
+++ src/5/a.rs
@@ -0,0 +1,62 @@
+use std::env;
+use std::fs;
+
+fn convert(key: i128, map: &Vec<(i128, i128, i128)>) -> i128 {
+    for (k, v, p) in map {
+        if key >= *v && key <= *v + *p {
+            return *k + (key - *v);
+        }
+    }
+    return key;
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let file_name: &String = &args[1];
+    let mut seeds: Vec<i128> = Vec::new();
+    let mut seed: Vec<(i128, i128, i128)> = Vec::new();
+    let mut soil: Vec<(i128, i128, i128)> = Vec::new();
+    let mut fertilizer: Vec<(i128, i128, i128)> = Vec::new();
+    let mut water: Vec<(i128, i128, i128)> = Vec::new();
+    let mut light: Vec<(i128, i128, i128)> = Vec::new();
+    let mut temp: Vec<(i128, i128, i128)>= Vec::new();
+    let mut humidity: Vec<(i128, i128, i128)> = Vec::new();
+    let mut current: &mut Vec<(i128, i128, i128)> = &mut seed;
+    let mut first: bool = true;
+    for line in fs::read_to_string(file_name).unwrap().lines() {
+        if first {
+            first = false;
+            line.split_whitespace().skip(1).map(|x| x.trim().parse::<i128>().unwrap()).for_each(|x| seeds.push(x));
+            continue;
+        }
+        if line.len() == 0 {
+            continue;
+        }
+        if line.starts_with("seed") {
+            current = &mut seed;
+        } else if line.starts_with("soil") {
+            current = &mut soil;
+        } else if line.starts_with("fertilizer") {
+            current = &mut fertilizer;
+        } else if line.starts_with("water") {
+            current = &mut water;
+        } else if line.starts_with("light") {
+            current = &mut light;
+        } else if line.starts_with("temp") {
+            current = &mut temp;
+        } else if line.starts_with("humidity") {
+            current = &mut humidity;
+        } else {
+            let data: Vec<i128> = line.split_whitespace().map(|x| x.trim().parse::<i128>().unwrap()).collect();
+            current.append(&mut vec![(data[0], data[1], data[2])]);
+        }
+    }
+    let mut low_plot: i128 = i128::MAX;
+    for s in seeds {
+        let plot = convert(convert(convert(convert(convert(convert(convert(s, &seed), &soil), &fertilizer), &water), &light), &temp), &humidity);
+        if plot < low_plot {
+            low_plot = plot;
+        }
+    }
+    println!("{}", low_plot);
+}
